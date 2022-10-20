9e3fad037778a91f3c0e9c7a870d4d70088aa12d
Sepid89 <s.jamshididana>
2022-10-20 11:29:42 +0200

    added Files

---
+++ Flutter-Simple-Calculator/flutter_calculator/lib/screen/main_screen.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+///
+import '../controller/theme_controller.dart';
+import '../controller/calculate_controller.dart';
+import '../utils/colors.dart';
+import '../widget/button.dart';
+
+class MainScreen extends StatelessWidget {
+  MainScreen({Key? key}) : super(key: key);
+
+  final List<String> buttons = [
+    "C",
+    "CE",
+    "%",
+    "/",
+    "9",
+    "8",
+    "7",
+    "x",
+    "6",
+    "5",
+    "4",
+    "-",
+    "3",
+    "2",
+    "1",
+    "+",
+    "0",
+    ".",
+    "",
+    "=",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    var controller = Get.find<CalculateController>();
+    var themeController = Get.find<ThemeController>();
+
+    return GetBuilder<ThemeController>(builder: (context) {
+      return Scaffold(
+        backgroundColor: themeController.isDark
+            ? DarkColors.scaffoldBgColor
+            : LightColors.scaffoldBgColor,
+        body: Column(
+          children: [
+            GetBuilder<CalculateController>(builder: (context) {
+              return outPutSection(themeController, controller);
+            }),
+            inPutSection(themeController, controller),
+          ],
+        ),
+      );
+    });
+  }
+
+  /// In put Section - Enter Numbers
+  Expanded inPutSection(
+      ThemeController themeController, CalculateController controller) {
+    return Expanded(
+        flex: 2,
+        child: Container(
+          padding: const EdgeInsets.all(3),
+          decoration: BoxDecoration(
+              color: themeController.isDark
+                  ? DarkColors.sheetBgColor
+                  : LightColors.sheetBgColor,
+              borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(30), topRight: Radius.circular(30))),
+          child: GridView.builder(
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: buttons.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4),
+              itemBuilder: (contex, index) {
+                switch (index) {
+
+                  /// CLEAR BTN
+                  case 0:
+                    return CustomButton(
+                        buttonTapped: () {
+                          controller.clearInputAndOutput();
+                        },
+                        color: themeController.isDark
+                            ? DarkColors.btnBgColor
+                            : LightColors.btnBgColor,
+                        textColor: themeController.isDark
+                            ? DarkColors.leftOperatorColor
+                            : LightColors.leftOperatorColor,
+                        text: buttons[index]);
+
+                  /// DELETE BTN
+                  case 1:
+                    return CustomButton(
+                        buttonTapped: () {
+                          controller.deleteBtnAction();
+                        },
+                        color: themeController.isDark
+                            ? DarkColors.btnBgColor
+                            : LightColors.btnBgColor,
+                        textColor: themeController.isDark
+                            ? DarkColors.leftOperatorColor
+                            : LightColors.leftOperatorColor,
+                        text: buttons[index]);
+
+                  /// EQUAL BTN
+                  case 19:
+                    return CustomButton(
+                        buttonTapped: () {
+                          controller.equalPressed();
+                        },
+                        color: themeController.isDark
+                            ? DarkColors.btnBgColor
+                            : LightColors.btnBgColor,
+                        textColor: themeController.isDark
+                            ? DarkColors.leftOperatorColor
+                            : LightColors.leftOperatorColor,
+                        text: buttons[index]);
+
+                  default:
+                    return CustomButton(
+                        buttonTapped: () {
+                          controller.onBtnTapped(buttons, index);
+                        },
+                        color: themeController.isDark
+                            ? DarkColors.btnBgColor
+                            : LightColors.btnBgColor,
+                        textColor: isOperator(buttons[index])
+                            ? LightColors.operatorColor
+                            : themeController.isDark
+                                ? Colors.white
+                                : Colors.black,
+                        text: buttons[index]);
+                }
+              }),
+        ));
+  }
+
+  /// Out put Section - Show Result
+  Expanded outPutSection(
+      ThemeController themeController, CalculateController controller) {
+    return Expanded(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(right: 20, top: 70),
+          child: Column(
+            children: [
+              Container(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  controller.userInput,
+                  style: TextStyle(
+                      color:
+                          themeController.isDark ? Colors.white : Colors.black,
+                      fontSize: 25),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                alignment: Alignment.bottomRight,
+                child: Text(controller.userOutput,
+                    style: TextStyle(
+                        color: themeController.isDark
+                            ? Colors.white
+                            : Colors.black,
+                        fontSize: 32)),
+              ),
+            ],
+          ),
+        ),
+      ],
+    ));
+  }
+
+  ///
+  bool isOperator(String y) {
+    if (y == "%" || y == "/" || y == "x" || y == "-" || y == "+" || y == "=") {
+      return true;
+    }
+    return false;
+  }
+}
